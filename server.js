2055b89021c9db31adf7d4af067920fa9a4a04e7
Dusing Chang <dusing.chang@mendix.com>
2026-06-12 16:07:20 +0200

    Remove unnecessary comments and clean up server.js

--- server.js
+++ server.js
@@ -1,6 +1,3 @@
-// 1. 載入 Node.js 內建的 http 模組。
-//    關鍵字提示：
-//    const、http、require、'http'
 const http = require('http');
 const { v4: uuidv4 } = require('uuid');
 const errorHandle = require('./errorHandle');
@@ -125,39 +122,3 @@ const requestListener = (req, res) => {
 
 const server = http.createServer(requestListener);
 server.listen(3000);
-
-// 3. 在 requestListener 裡，先設定 response header。
-//    要使用的方法：
-//    res.writeHead(...)
-//    你需要自己放入兩個參數：
-//    第一個參數：HTTP 狀態碼，成功時使用 200
-//    第二個參數：一個物件，用來設定 Content-Type 是 text/plain
-
-// 4. 接著在 requestListener 裡，寫入要回傳的文字內容。
-//    要使用的方法：
-//    res.write(...)
-//    你需要自己放入一個參數：
-//    一段字串，例如想讓瀏覽器看到的文字
-
-// 5. 最後在 requestListener 裡，結束 response。
-//    要使用的方法：
-//    res.end()
-//    這個方法不一定要放參數。
-//    它的作用是告訴瀏覽器：伺服器已經回應完了。
-
-// 6. 建立 server。
-//    要使用的方法：
-//    http.createServer(...)
-//    你需要自己放入一個參數：
-//    第 2 步建立好的 requestListener
-
-// 7. 讓 server 開始監聽 port。
-//    要使用的方法：
-//    server.listen(...)
-//    你需要自己放入一個參數：
-//    port number，這次練習使用 3000
-
-// 8. 寫完後，在 terminal 執行：
-//    node server.js
-//    然後打開瀏覽器進入：
-//    http://localhost:3000
